ef8587c88ef07b2b66fb32c648c0dfc641546c19
Babitapaul <babitap495@gmail.com>
2020-05-27 21:02:47 -0400

    login

--- app/src/main/java/com/example/park_mate/Login.java
+++ app/src/main/java/com/example/park_mate/Login.java
@@ -1,11 +1,11 @@
 package com.example.park_mate;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
